4cea4011cf4fb58eabba48162faa56d3f3f6f640
Natan Vilchis <contacto@natanvilchis.org>
2019-09-04 00:36:32 -0500

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,6 @@
 #coding: utf-8
 import sys
+import os
 global semilla, a, c, m
 
 semilla=input("Semilla: ")
@@ -10,7 +11,8 @@ semilla=int(temp)
 m=int(eval(input("m: ")))
 a=int(eval(input("a: ")))
 c=int(eval(input("c: ")))
-
+steps=int(eval(input("steps: ")))
+n=int(input("n: "))
 def aleatorio():
 	global semilla, a, c,m
 	semilla=(a*semilla+c)%m
@@ -19,9 +21,10 @@ def aleatorio():
 temp=""
 x=0
 txt=open(".localkey","w")
-for i in range(25):
+for i in range(n):
 	z=aleatorio()
-	temp_char=chr(int(str(z)[:2]))
+	temp_char=chr(steps+int(str(z)[:2]))
 	txt.write(temp_char)
 	x+=1
 txt.close()
+#os.system("xclip -sel cli < .localkey")
