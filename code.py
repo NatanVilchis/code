70de7e0aa9db74fde4a75c42b8c7f6c724e206c6
Natan Vilchis <contacto@natanvilchis.org>
2020-09-01 19:25:50 -0500

    Commit

--- code.py
+++ code.py
@@ -1,39 +1,48 @@
 #coding: utf-8
 import os
-
+import platform
 global semilla, a, c, m
 
-semilla=input("Semilla: ")
-temp=""
+OS = {
+  "Linux": "xclip -sel cli < .localkey",
+  ""   
+}
+
+semilla = input("Semilla: ")
+temp = ""
 for letra in semilla:
-	temp+=str(ord(letra))
-semilla=int(temp)
-m=int(eval(input("m: ")))
-a=int(eval(input("a: ")))
-c=int(eval(input("c: ")))
-steps=int(eval(input("steps: ")))
-n=int(input("n: "))
-OS_choices=["desktop","termux","d","t"]
-OS=""
-while not OS in OS_choices:
-	OS=input("desktop|termux: ")
+  temp += str(ord(letra))
+
+semilla = int(temp)
+m = int(eval(input("m: ")))
+a = int(eval(input("a: ")))
+c = int(eval(input("c: ")))
+steps = int(eval(input("steps: ")))
+n = int(input("n: "))
 
 def aleatorio():
-	global semilla, a, c,m
-	semilla=(a*semilla+c)%m
-	return semilla
+  global semilla, a, c,m
+  semilla = (a*semilla+c)%m
+  return semilla
 
-temp=""
-x=0
-txt=open(".localkey","w")
+temp = ""
+x = 0
+txt = open(".localkey","w")
 for i in range(n):
-	z=aleatorio()
-	temp_char=chr(steps+int(str(z)[:2]))
-	txt.write(temp_char)
-	x+=1
+  z = aleatorio()
+  temp_char = chr(steps+int(str(z)[:2]))
+  txt.write(temp_char)
+  temp += temp_char 
+  x += 1
 txt.close()
-if OS=="termux":
-	os.system("cat .localkey | termux-clipboard-set")
-else:
-	os.system("xclip -sel cli < .localkey")
+
+
+
+if platform.uname().system == "Linux":
+  if platform.uname().machine == "x86_64":
+    os.system("xclip -sel cli < .localkey")
+  elif platform.uname().machine == "aarch64":
+    os.system("cat .localkey | termux-clipboard-set")
+elif platform.uname().system == "Windows":
+  os.system("echo " + temp + "| clip")
 os.remove(".localkey")
